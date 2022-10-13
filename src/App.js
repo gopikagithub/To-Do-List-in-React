90199d67baecbe41a05df88340dacdcdd48d0ed3
GopzGit <gopikanath45@gmail.com>
2022-10-13 11:04:47 +0400

    TO DO List in reactusing class completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React,{Component} from'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component{
+
+  state={input:"",items:[]};
+
+  handleChange=(event)=>{
+    this.setState({input:event.target.value });
+  };
+  
+  storeItems=(event)=>{
+      event.preventDefault();
+      const {input}=this.state;
+      this.setState({items:[...this.state.items,input],input:""});
+  };
+
+  deleteItem=(index)=>{
+        const allItems=this.state.items;
+        allItems.splice(index,1);
+        this.setState({items:allItems})
+         
+
+  }
+
+  render(){
+
+    const{input,items}=this.state;
+    console.log(items);
+
+    return(
+      <div className="todo-container">
+
+        <h1>Todo List</h1>
+
+        <form className="input-section" onSubmit={this.storeItems}>
+          <input type="text" value={input}  onChange={this.handleChange}placeholder="Enter Items..."/>
+        </form>
+
+        <ul>
+            {items.map((data,index)=>(
+              <li key={index}>{data}
+              <i class="fa-solid fa-pen-to-square"></i><i className="fas fa-trash-alt" onClick={()=>this.deleteItem(index)}></i></li>
+            ))}
+            
+        </ul>
+              
+      </div>
+
+  );}
+
 }
 
 export default App;
